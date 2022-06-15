d2a634107c25744d130b44962733c9ccdad7a35d
Wonnie <37063322+haruu19@users.noreply.github.com>
2022-06-16 00:51:25 +0900

    회원가입 뷰

--- src/main/java/com/studyhalle/account/AccountController.java
+++ src/main/java/com/studyhalle/account/AccountController.java
@@ -9,6 +9,7 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signupForm(Model model) {
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
 }
--- src/main/java/com/studyhalle/config/SecurityConfig.java
+++ src/main/java/com/studyhalle/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.studyhalle.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    /**
+     * Spring Security 5.7.x 부터 WebSecurityConfigurerAdapter 는 Deprecated.
+     * -> SecurityFilterChain, WebSecurityCustomizer 를 상황에 따라 빈으로 등록해 사용한다.
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
@@ -20,5 +26,12 @@ public class SecurityConfig {
                 .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
