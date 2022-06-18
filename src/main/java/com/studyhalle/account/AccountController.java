18b9dfdb8515edbfa61ad4e663bfff82056a279b
Wonnie <37063322+haruu19@users.noreply.github.com>
2022-06-18 23:35:53 +0900

    회원가입 폼 서브밋 처리

--- src/main/java/com/studyhalle/account/AccountController.java
+++ src/main/java/com/studyhalle/account/AccountController.java
@@ -1,6 +1,9 @@
 package com.studyhalle.account;
 
+import com.studyhalle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,6 +19,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     // Form 객체의 이름을 받을 때 실행된다.
     @InitBinder("signUpForm")
@@ -35,6 +40,25 @@ public class AccountController {
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO encoding 해야함
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디할래, 회원가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+                "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
 
         // TODO 회원가입 처리
         return "redirect:/";
