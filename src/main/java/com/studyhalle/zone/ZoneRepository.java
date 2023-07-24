c34d8b3beb806e12bb7c8b3f61cfcb03d41fc9c1
haruu <haruu0419@gmail.com>
2023-07-24 11:35:47 +0900

    Zone domain and init data

---
+++ src/main/java/com/studyhalle/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.studyhalle.zone;
+
+import com.studyhalle.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
